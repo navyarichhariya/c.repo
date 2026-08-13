8734a1b1a5e3f178a890656554f01420609d9f9e
navya richhariya <asahinugget270@gmail.com>
2026-08-13 14:12:47 +0530

    Add files via upload

---
+++ prog1_day1.c
@@ -0,0 +1,37 @@
+/*Q1: Write a program to input two numbers and display their sum.
+
+
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+
+
+ 
+#include<stdio.h>
+   int main()
+{
+        int a,b;
+        int sum;
+        scanf("%d%d",&a,&b);
+        sum= a+b;
+        printf("sum = %d",sum);
+        return 0;
+		
+ }
+ 
+ 
+ 
+/*C:\Users\asus\OneDrive\Desktop\cprog>test.exe
+10 20
+sum = 30
+*/
+
