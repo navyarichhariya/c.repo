48f48174bde4380a1bfd4ce99aac12fa3895748c
navya richhariya <asahinugget270@gmail.com>
2026-09-16 00:36:01 +0530

    Add files via upload

---
+++ prog1_day6.c
@@ -0,0 +1,40 @@
+/*Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+7 is odd
+
+Input 2:
+12
+Output 2:
+12 is even
+
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is an even number.\n", num);
+    } else {
+        printf("%d is an odd number.\n", num);
+    }
+
+    return 0;
+}
+
+
+
+/*
+Enter an integer: 4
+4 is an even number.
+
+*/
---
+++ prog2_day6.c
@@ -0,0 +1,48 @@
+/*
+Q12: Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+
+Sample Test Cases:
+Input 1:
+-5
+Output 1:
+Negative
+
+Input 2:
+0
+Output 2:
+Zero
+
+Input 3:
+10
+Output 3:
+Positive
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num >= 0) {
+        if (num == 0) {
+            printf("%d is zero.\n", num);
+        } else {
+            printf("%d is a positive number.\n", num);
+        }
+    } else {
+        printf("%d is a negative number.\n", num);
+    }
+
+    return 0;
+}
+
+
+/*
+Enter an integer: 6
+6 is a positive number.
+/*
